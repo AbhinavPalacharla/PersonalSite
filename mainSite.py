0e27bb5d404fb2a4d6b9bcbff83d0fee22db3e28
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2020-01-04 22:55:50 -0800

    initial commit

---
+++ mainSite.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def landing_page():
+	return render_template('landingPage.html', name='hello')
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
